ff00cac5b05fa0d5b4087ae0eacac2fe7eec4398
Alex <amackay515@gmail.com>
2020-06-05 00:26:54 -0400

    some comments

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,5 @@
+//The player class has all of the variables associated with each player
+//and has some mechanical functions like movement ans shooting
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -6,21 +8,21 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Player {
-    private double speedX,speedY,defaultSpeed;
-    private double jumpMod;
-    private int numOfBullets;
+    private double speedX,speedY,defaultSpeed;//speeds
+    private double jumpMod;//modifier for how high you jump
+    private int numOfBullets;//ammo
     private int lives;
     private int jumpCounter,invincibleCounter;
-    private String dir;
-    private int x,y;
+    private String dir;//direction currently facing or aiming
+    private int x,y;//location
     private boolean onGround,onWall,wallJumping,invincible;
-    private Rectangle hitBox,feetBox,headBox,leftBox,rightBox,shieldHitBox;
-    private LinkedList<Bullet> bullets;
-    private double bulletSpeedMod;
+    private Rectangle hitBox,feetBox,headBox,leftBox,rightBox,shieldHitBox; //hitboxes
+    private LinkedList<Bullet> bullets;//fired bullets
+    private double bulletSpeedMod;//modifier for how fast bullets move
     private double shieldEnergy;
     private double maxShieldEnergy;
-    private double shieldRechargeMod;
-    private double shieldRadius;
+    private double shieldRechargeMod;//how quickly shield recharges
+    private double shieldRadius;//size of shield
     private Boolean activeShield;
     private ArrayList<Image> frames;
     private int currentFrame;
@@ -31,6 +33,7 @@ public class Player {
 
     public Player(int playerNum){
         loadPics();
+        //starting values for all variables
         lives=2;
         jumpCounter=0;
         invincibleCounter=0;
@@ -49,8 +52,10 @@ public class Player {
         shieldRechargeMod=1;
         bulletSpeedMod=1;
         if (playerNum==1){
+            //spawn point
             x=200;
             y=125;
+            //controls
             rightBtn=KeyEvent.VK_D;
             leftBtn=KeyEvent.VK_A;
             upBtn=KeyEvent.VK_W;
@@ -73,6 +78,7 @@ public class Player {
             jumpBtn=KeyEvent.VK_J;
             dir="left";
         }
+        //initialize hitboxes
         hitBox =new Rectangle(x,y,15,40);
         feetBox=new Rectangle(x,y+35,15,5);
         headBox=new Rectangle(x,y-2,15,8);
@@ -80,17 +86,13 @@ public class Player {
         rightBox=new Rectangle(x+15,y,5,35);
         shieldHitBox=new Rectangle(x+7-(int)shieldRadius,y+20-(int)shieldRadius,2*(int)shieldRadius,2*(int)shieldRadius);
         bullets=new LinkedList<>();
-
-        shieldRechargeMod=1;
         currentFrame=1;
         currentPowers=new ArrayList<>();
-        //addPower("Longer Shield");
-
-
     }
 
     public void loadPics(){
         bulletPics=new ArrayList<>();
+        //create the frames for bullets
         for (int i=1;i<9;i++) {
             Image bulletPic;
             bulletPic = new ImageIcon("Bullet/bullet"+i+".png").getImage();
@@ -110,9 +112,9 @@ public class Player {
     }
 
     public void move(){
-        x+=speedX;
+        x+=speedX;//move each direction
         y+=speedY;
-        if (invincible){
+        if (invincible){//invincibility is on a timer
             invincibleCounter+=1;
         }
         if (invincibleCounter>=280){
@@ -120,7 +122,7 @@ public class Player {
             invincible=false;
         }
     }
-    public void jump(){
+    public void jump(){//start moving upward
         speedY=-7;
         onGround=false;
     }
@@ -128,6 +130,7 @@ public class Player {
     public void shoot(){
         Image bulletPic =null;
         if (dir=="right"){
+            //choose which frame to use depending on which direction the bullet is fired at
             bulletPic=bulletPics.get(4);
         }
         if (dir=="left"){
@@ -151,11 +154,12 @@ public class Player {
         if (dir=="down/left"){
             bulletPic=bulletPics.get(1);
         }
-        bullets.add(new Bullet(dir,x,y,7*bulletSpeedMod,bulletPic));
+        bullets.add(new Bullet(dir,x,y,7*bulletSpeedMod,bulletPic));//make new bullet
         numOfBullets-=1;
     }
 
     public void setHitboxes(){
+        //refresh hitboxes to keep up with movement, etc
         hitBox=new Rectangle(x,y,15,40);
         feetBox=new Rectangle(x,y+35,15,5);
         leftBox=new Rectangle(x-5,y,5,35);
@@ -166,37 +170,38 @@ public class Player {
 
     }
 
-    public void addPower(String name){
+    public void addPower(String name){//picks up an item
         if (name=="Jump Boost"){
-            jumpMod=0.5;
+            jumpMod=0.5;//alters gravity to be less forceful
         }
         else if (name=="Bigger Shield"){
-            shieldRadius*=1.5;
+            shieldRadius*=1.5;//increases size
         }
         else if (name=="Longer Shield"){
-            System.out.println("ooga");
-            shieldRechargeMod=0.4;
+            shieldRechargeMod=0.4;//will deplete slower
         }
         else if (name=="Bullet Pack"){
-            numOfBullets=Math.min(numOfBullets+3,6);
+            numOfBullets=Math.min(numOfBullets+3,6);//get more bullets but can't go over 6
         }
         else if (name=="Extra Life"){
             lives+=1;
         }
         else if (name=="Fast Bullets"){
-            bulletSpeedMod=1.5;
+            bulletSpeedMod=1.5;//bullets mvoe faster
         }
         else if (name=="Single Bullet"){
-            numOfBullets=Math.min(numOfBullets+1,6);
+            numOfBullets=Math.min(numOfBullets+1,6);//get an extra bullet
         }
         else if (name=="Invincibility"){
             invincible=true;
         }
         if (name!="Extra Life" && name!="Bullet Pack" && name!="Single Bullet" && name!="Invincibility") {
+            //these powerups can be receivec more than once as they are not permanents
             currentPowers.add(name);
         }
     }
 
+    //accessor methods
     public int getX(){
         return x;
     }
@@ -257,36 +262,20 @@ public class Player {
         return maxShieldEnergy;
     }
     public double getShieldRadius(){return shieldRadius;}
-    public void setShieldRadius(double n){shieldRadius=n;}
     public Rectangle getHitBox(){
         return hitBox;
-    }
-    public void setHitBox(Rectangle rect){
-        hitBox =rect;
     }
     public Rectangle getFeetBox(){
         return feetBox;
     }
-    public void setFeetBox(Rectangle rect){
-        feetBox=rect;
-    }
     public Rectangle getHeadBox(){
         return headBox;
-    }
-    public void setHeadBox(Rectangle rect){
-        headBox=rect;
     }
     public Rectangle getLeftBox(){
         return leftBox;
     }
-    public void setLeftBox(Rectangle rect){
-        leftBox=rect;
-    }
     public Rectangle getRightBox(){
         return rightBox;
-    }
-    public void setRightBox(Rectangle rect){
-        rightBox=rect;
     }
     public int getNumOfBullets(){
         return numOfBullets;
@@ -313,13 +302,11 @@ public class Player {
     public double getJumpMod(){
         return jumpMod;
     }
-    public void setJumpMod(double n){jumpMod=n;}
     public ArrayList<String> getCurrentPowers(){
         return  currentPowers;
     }
     public double getShieldRechargeMod(){return shieldRechargeMod;}
     public double getDefaultSpeed(){return defaultSpeed;}
-    public void setDefaultSpeed(double n){defaultSpeed=n;}
     public ArrayList<Image> getBulletPics(){return bulletPics;}
     public boolean isOnWall(){return onWall;}
     public void setOnWall(boolean b){onWall=b;}
